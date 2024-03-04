ca0490d457ee2698a309a869b22be6aa909dd0da
Paweł Trela <pawel.trela@fingoweb.com>
2024-03-04 09:21:21 +0100

    fix: delete keyvalues and area declaration

--- src/managers/AdManager/AdManager.ts
+++ src/managers/AdManager/AdManager.ts
@@ -1,6 +1,5 @@
 import { AD_PIXEL_DEPS_URL } from 'consts/dlApi';
 import { getProductsIds } from 'managers/AdManager/AdManager.utils';
-import { EAreas } from 'types/areas';
 import { TPages } from 'types/pages';
 
 class AdManager {
@@ -15,25 +14,7 @@ class AdManager {
     }
   }
 
-  private mapPageToArea = (page: TPages) => {
-    const areas: Record<TPages, EAreas> = {
-      PRODUCTS_LIST: EAreas.LISTING,
-      PRODUCT_DETAILS: EAreas.PRODUCT_CARD,
-    };
-
-    return areas[page];
-  };
-
   public injectAdnPixelScript = () => {
-    const area = this.page ? this.mapPageToArea(this.page) : null;
-
-    if (window.dlApi && dlApi.fetchNativeAd) {
-      dlApi.area = area || 'ros';
-      dlApi.addKeyValue('offer_ids', this.productsIds.toString());
-
-      return;
-    }
-
     const adPixelDepsScript = document.createElement('script');
 
     adPixelDepsScript.src = AD_PIXEL_DEPS_URL;
